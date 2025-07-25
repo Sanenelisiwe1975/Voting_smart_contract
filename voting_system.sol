54c355f5b1853d6281bec1ecf3fb55ba6f567855
Sanelisiwe1975 <simatusanelisiwe@gmail.com>
2025-07-25 19:30:15 +0200

    Update voting_system.sol
    Ownable

--- voting_system.sol
+++ voting_system.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
 
+contract Ownable {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner");
+        _;
+    }
+}
