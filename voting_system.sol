a59c47c88dc77b90af9838f0dc00f8059593a997
Singa_N <123820632+SingaFirstLast@users.noreply.github.com>
2025-07-25 19:36:50 +0200

    Update voting_system.sol
    Singa added the structs & mappings.

--- voting_system.sol
+++ voting_system.sol
@@ -13,3 +13,28 @@ contract Ownable {
         _;
     }
 }
+
+contract VotingSystem is Ownable {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    mapping(address => bool) public voters;
+    uint public candidatesCount;
+
+    event CandidateAdded(uint indexed candidateId, string name);
+    event Voted(address indexed voter, uint indexed candidateId);
+
+    function addCandidate(string calldata _name) public onlyOwner {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate({
+            id: candidatesCount,
+            name: _name,
+            voteCount: 0
+        });
+        emit CandidateAdded(candidatesCount, _name);
+    }
+
